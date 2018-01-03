f63f1ece0d99da455f0763e373858efceceb6850
Blake Jones <bjones325@gatech.edu>
2018-01-02 23:48:23 -0500

    Initial Commit

---
+++ NeuralNet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NeuralNet
+//
+//  Created by Blake Jones on 1/2/18.
+//  Copyright © 2018 Blake Jones. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
